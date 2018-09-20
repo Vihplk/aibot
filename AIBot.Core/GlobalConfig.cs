4a3b8492f612f8a8561f07ecd12be021aa75c30a
Dinusha <1000seeker@gmail.com>
2018-09-20 15:47:11 +0530

    CSS enhancements

--- AIBot.Core/GlobalConfig.cs
+++ AIBot.Core/GlobalConfig.cs
@@ -7,7 +7,7 @@
         public const string Audience = "http://localhost:65000";
 
         public const string ConnectionString =
-            "Data Source=DESKTOP-LE44UH9;Initial Catalog=AIBot;Integrated Security=True";
+            @"Data Source=DESKTOP-RQBOHL0\SQLEXPRESS;Initial Catalog=AIBot;Integrated Security=True";
 
         public const string TwaipKey = "sQIrQkX7LMIF5tWf3ItVzFmjL2l3v4Zpk/k+s2cT5hjHO4BE38CSDEY5koPc1X/ImohAJYsP0uacuiTRdJVCPw==";
         public const string TwaipApiEndpoint = "https://api.twinword.com/api/v6/text/similarity/";
