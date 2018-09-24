db62ffec774cee636494e3bcde7e04b8b588f9d3
Dinusha <1000seeker@gmail.com>
2018-09-24 09:11:13 +0530

    image path changed

--- AIBot.Game/AIBot.Game/Globalconfig.cs
+++ AIBot.Game/AIBot.Game/Globalconfig.cs
@@ -2,7 +2,7 @@
 {
     public class Globalconfig
     {
-        public const string ImageLocation = @"E:\projects\Bot\AIBot\AIBot.Game\AIBot.Game\Images\";
+        public const string ImageLocation = @"E:\AIBot\fork\aibot\AIBot.Game\AIBot.Game\Images";
         public const string ApiEndPoint = "http://localhost:5500";
     }
 }
